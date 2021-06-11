ec3d5025812441518ffa91a03e0ba44728a0b6f5
KamFor <kester.kamil@gmail.com>
2021-06-11 19:21:41 +0200

    Navbar finished

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import {
+	Nav,
+	NavbarContainer,
+	NavLogo,
+	MobileIcon,
+	NavMenu,
+	NavItem,
+	NavLinks,
+	NavBtn,
+	NavBtnLink
+} from './NavbarElements';
 
 const index = () => {
 	return (
 		<>
 			<Nav>
 				<NavbarContainer>
-					<NavLogo>dollar</NavLogo>
+					<NavLogo to="/">dolla</NavLogo>
+					<MobileIcon>
+						<FaBars />
+					</MobileIcon>
+					<NavMenu>
+						<NavItem>
+							<NavLinks to="about">About</NavLinks>
+						</NavItem>
+
+						<NavItem>
+							<NavLinks to="discover">Discover</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to="services">Services</NavLinks>
+						</NavItem>
+						<NavItem>
+							<NavLinks to="sign up">Sign Up</NavLinks>
+						</NavItem>
+					</NavMenu>
+					<NavBtn>
+						<NavBtnLink to="signin">Sign In</NavBtnLink>
+					</NavBtn>
 				</NavbarContainer>
 			</Nav>
 		</>
